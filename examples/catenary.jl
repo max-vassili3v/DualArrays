18a24c532c2eebd65766a20f0794448d355dec05
Max Vassiliev <vassiliev.max@gmail.com>
2026-01-22 19:47:09 +0000

    use midpoints

--- examples/catenary.jl
+++ examples/catenary.jl
@@ -1,4 +1,4 @@
-using DualArrays, LinearAlgebra
+using DualArrays, LinearAlgebra, Plots
 
 """
 Solving the Catenary Problem using DualArrays.jl
@@ -10,19 +10,28 @@ gradient descent and DualArrays.jl
 
 function L(y, h, alpha, beta)
     """
-    For each element y_i, approximate L(y, y') as
-    L(y_i, (y_{i+1} - y_i) / h), returning a vector of L values.
+    Evaluate functional with boundary conditions (alpha, beta)
+    By approximating y' using finite differences
     """
-    dy = ([y; beta] .- [alpha; y]) / h
-    return y .* sqrt.(1 .+ dy[2:end] .^ 2)
+    y_ext = [alpha; y; beta]
+    dy = (y_ext[2:end] - y_ext[1:end-1]) / h
+    y_mid = (y_ext[1:end-1] + y_ext[2:end]) / 2
+    return y_mid .* sqrt.(1 .+ dy.^2)
 end
 
-function learn_catenary(; h = 0.1, alpha = cosh(-1), beta = cosh(1), epochs = 1000, lr = 0.02)
-    y = ones(Int(2/ h) - 1) * (alpha + beta) / 2
+function learn_catenary(;n = 20, h = 0.1, alpha = cosh(-1), beta = cosh(1), epochs = 10000, lr = 0.01)
+    y = ones(n) * (alpha + beta) / 2
     for _ = 1:epochs
         jac = jacobian(y -> L(y, h, alpha, beta), y, id="banded")
         grads = h * sum(jac, dims=1)
         y -= lr * vec(grads)
     end
     return y
+end
+
+function plot_solution(n)
+    x = LinRange(-1, 1, n)
+    y = learn_catenary(n = n)
+    plot(x, y, label = "Approximation")
+    plot!(x, cosh.(x), label = "Exact Solution")
 end
