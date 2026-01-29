3e30ee70b1b41145807f166b8165bfc5871fd47b
Max Vassiliev <vassiliev.max@gmail.com>
2026-01-29 11:35:57 +0000

    clarify comment

--- examples/jacobians.jl
+++ examples/jacobians.jl
@@ -4,8 +4,8 @@
 #
 # We observe that for vector-valued functions with a sparse jacobian,
 # performance is significantly better using DualArrays.jl
-# i.e if an n x n matrix has O(n) nonzero entries, then
-# DualArrays.jl computes the jacobian in O(n) time.
+# i.e if the n x n jacobian has O(n) nonzero entries, then
+# DualArrays.jl computes it in O(n) time.
 #
 ###
 
