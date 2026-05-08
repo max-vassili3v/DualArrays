05eb51a2e4e206ef71fd64ee4113ce3b28e4af39
Sheehan Olver <solver@mac.com>
2026-05-08 11:41:27 +0100

    Update indexing.jl

--- src/indexing.jl
+++ src/indexing.jl
@@ -10,44 +10,6 @@ sparse_getindex(D::Diagonal, k::Integer, ::Colon) = OneElement(D.diag[k], k, siz
 sparse_getindex(D::Diagonal, ::Colon, j::Integer) = OneElement(D.diag[j], j, size(D, 1))
 sparse_getindex(d::DualArray, args...) = d[args...]
 
-# We need a system of indexing that takes two tuples
-# of length N and M. We must then return a ArrayOperator whose new input and output dimensions
-# are inferred from how many of the arguments in each respective tuple are integers.
-
-# For the purposes of DualArrays.jl, we only need to index the input dimensions
-#
-# Example:
-#
-# Consider a DualVector with standard matrix Jacobian (ArrayOperator{2, T, 1, 1}):
-#
-# d = a + Bϵ
-#
-# If we index d[1], we have:
-#
-# d[1] = a[1] + B[1, :]ϵ
-#
-# Where B[1, :] indexes the input dimension of B
-Idx = Union{Int, Colon, AbstractUnitRange}
-count_ints(t::Tuple) = count(x -> x isa Int, t)
-
-function getindex(t::ArrayOperator{N, M, <:Any, <:Any}, i::NTuple{N, Idx}, j::NTuple{M, Idx}) where {N, M}
-    # new value of M is inferred from the ArrayOperator constructor and the order of
-    # indexing the underlying array.
-    newN = N - count_ints(i)
-    ArrayOperator{newN}(sparse_getindex(t.data, i..., j...))
-end
-
-function getindex(t::ArrayOperator{N, M, <:Any, <:Any}, i::NTuple{N, Idx}, ::Colon) where {N, M}
-    # new value of M is inferred from the ArrayOperator constructor and the order of
-    # indexing the underlying array.
-    newN = N - count_ints(i)
-    ArrayOperator{newN}(sparse_getindex(t.data, i..., ntuple(_ -> Colon(), M)...))
-end
-# Integer indexing always returns a scalar.
-getindex(t::ArrayOperator, i::Vararg{Int}) = getindex(t.data, i...)
-# Indexing with only slices/ranges returns a similar tensor
-getindex(t::ArrayOperator{N, M, <:Any, <:Any}, i::Vararg{Union{Colon, UnitRange}}) where {N, M} = ArrayOperator{N}(sparse_getindex(t.data, i...))
-
 
 # We need a system of indexing that takes two tuples
 # of length N and M. We must then return a ArrayOperator whose new input and output dimensions
